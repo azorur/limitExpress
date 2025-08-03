a3589048555c03722ad5bbdb9c23893afc7dd29e
Azorur <azorur159@gmail.com>
2025-08-03 21:27:15 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>극한퀵서비스</h1>
+        <p>현재 홈페이지 리뉴얼 중입니다.</p>
+        <img
+          src="https://sdmntprcentralus.oaiusercontent.com/files/00000000-5794-61f5-b6e8-c2d0ce78a534/raw?se=2025-08-03T13%3A06%3A13Z&sp=r&sv=2024-08-04&sr=b&scid=8e768162-eda9-54e0-9684-730736773912&skoid=c953efd6-2ae8-41b4-a6d6-34b1475ac07c&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-08-03T12%3A02%3A37Z&ske=2025-08-04T12%3A02%3A37Z&sks=b&skv=2024-08-04&sig=sf1lL2xQtUziqxsimmg4568rGmIlwKsqv%2Bt2i/zXKow%3D"
+          className="Main-image"
+          alt="Quick Service"
+        />
+        <p>빠르고 안전한 퀵서비스, 극한퀵서비스에서 곧 만나보세요.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
